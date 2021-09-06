a1bb2122d265cd2779b9bdbcaff6edd67693a52c
Evgenii Semenov <eo.semenoff@gmail.com>
2021-09-06 17:39:39 +0300

    Database checks added

--- src/test/java/test_suit/DB_Test.java
+++ src/test/java/test_suit/DB_Test.java
@@ -2,6 +2,7 @@ package test_suit;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.*;
 import pageobject.TourPurchasePage;
 import tools.DataHelper;
@@ -22,7 +23,7 @@ public class DB_Test {
     }
 
     @BeforeEach
-    void browserSetUp(){
+    void browserSetUp() {
         open("http://localhost:8080/");
         tourPurchasePage = new TourPurchasePage();
     }
@@ -39,21 +40,71 @@ public class DB_Test {
 
     @Test
     @DisplayName("Не должен сохранять номер карты в БД при заказе со страницы оплаты")
-    void shouldNotSaveCreditIdOnPaymentPageTest() {
+    void shouldNotSaveCreditIdOnPaymentPageTest() throws InterruptedException {
         var paymentPage = tourPurchasePage.payForTour();
         var approvedPayment = DataHelper.approvedPayment(DataHelper.randomPlusMonth());
-        paymentPage.successfulSendingForm(approvedPayment.getCardNumber(), approvedPayment.getMonth(),
+        paymentPage.fillAndSendPaymentInfo(approvedPayment.getCardNumber(), approvedPayment.getMonth(),
+                approvedPayment.getYear(), approvedPayment.getCardHolder(), approvedPayment.getCvv());
+        paymentPage.anyNotification();
+        assertEquals("null", DatabaseTool.getCreditId());
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Не должен сохранять номер карты в БД при заказе со страницы оформления кредита")
+    void shouldNotSaveCreditIdOnCreditPageTest() {
+        var creditPage = tourPurchasePage.buyWithCredit();
+        var approvedPayment = DataHelper.approvedPayment(DataHelper.randomPlusMonth());
+        creditPage.fillAndSendPaymentInfo(approvedPayment.getCardNumber(), approvedPayment.getMonth(),
                 approvedPayment.getYear(), approvedPayment.getCardHolder(), approvedPayment.getCvv());
         assertEquals("null", DatabaseTool.getCreditId());
     }
 
     @Test
-    @DisplayName("Не должен сохранять номер карты в БД при заказе со страницы оформления кредита")
-    void shouldNotSaveCreditIdOnCreditPageTest() {
+    @SneakyThrows
+    @DisplayName("Должен сохранять платеж с действующей карты в БД как одобренный при заказе со страницы оплаты")
+    void shouldApprovePaymentsWithApprovedCardOnPaymentPageTest() {
+        var paymentPage = tourPurchasePage.payForTour();
+        var approvedPayment = DataHelper.approvedPayment(DataHelper.randomPlusMonth());
+        paymentPage.fillAndSendPaymentInfo(approvedPayment.getCardNumber(), approvedPayment.getMonth(),
+                approvedPayment.getYear(), approvedPayment.getCardHolder(), approvedPayment.getCvv());
+        paymentPage.anyNotification();
+        assertEquals("APPROVED", DatabaseTool.getPaymentStatus());
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Должен сохранять платеж с недействительной карты в БД как отклоненный при заказе со страницы оплаты")
+    void shouldDeclinePaymentsWithDeclinedCardOnPaymentPageTest() {
+        var paymentPage = tourPurchasePage.payForTour();
+        var declinedPayment = DataHelper.declinedPayment(DataHelper.randomPlusMonth());
+        paymentPage.fillAndSendPaymentInfo(declinedPayment.getCardNumber(), declinedPayment.getMonth(),
+                declinedPayment.getYear(), declinedPayment.getCardHolder(), declinedPayment.getCvv());
+        paymentPage.anyNotification();
+        assertEquals("DECLINED", DatabaseTool.getPaymentStatus());
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Должен сохранять платеж с действующей карты в БД как одобренный при заказе со страницы оформления кредита")
+    void shouldApprovePaymentsWithApprovedCardOnCreditPageTest() {
         var creditPage = tourPurchasePage.buyWithCredit();
         var approvedPayment = DataHelper.approvedPayment(DataHelper.randomPlusMonth());
-        creditPage.successfulSendingForm(approvedPayment.getCardNumber(), approvedPayment.getMonth(),
+        creditPage.fillAndSendPaymentInfo(approvedPayment.getCardNumber(), approvedPayment.getMonth(),
                 approvedPayment.getYear(), approvedPayment.getCardHolder(), approvedPayment.getCvv());
-        assertEquals("null", DatabaseTool.getCreditId());
+        creditPage.anyNotification();
+        assertEquals("APPROVED", DatabaseTool.getCreditStatus());
+    }
+
+    @Test
+    @SneakyThrows
+    @DisplayName("Должен сохранять платеж с недействительной карты в БД как отклоненный при заказе со страницы оформления кредита")
+    void shouldDeclinePaymentsWithDeclinedCardOnCreditPageTest() {
+        var creditPage = tourPurchasePage.buyWithCredit();
+        var declinedPayment = DataHelper.declinedPayment(DataHelper.randomPlusMonth());
+        creditPage.fillAndSendPaymentInfo(declinedPayment.getCardNumber(), declinedPayment.getMonth(),
+                declinedPayment.getYear(), declinedPayment.getCardHolder(), declinedPayment.getCvv());
+        creditPage.anyNotification();
+        assertEquals("DECLINED", DatabaseTool.getCreditStatus());
     }
 }
--- src/test/java/tools/DatabaseTool.java
+++ src/test/java/tools/DatabaseTool.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseTool {
+
     public static String getCreditId() {
         var runner = new QueryRunner();
         var creditId = new String();
@@ -18,10 +19,32 @@ public class DatabaseTool {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-            return creditId;
-        }
+        return creditId;
     }
 
+    public static String getCreditStatus() {
+        var creditStatusSQL = "SELECT status FROM credit_request_entity WHERE created = (SELECT max(created) FROM credit_request_entity);";
+        return getValue(creditStatusSQL);
+    }
+
+    public static String getPaymentStatus() {
+        var creditStatusSQL = "SELECT status FROM payment_entity WHERE created = (SELECT max(created) FROM payment_entity);";
+        return getValue(creditStatusSQL);
+    }
+
+    public static String getValue(String request) {
+        var runner = new QueryRunner();
+        var value = new String();
+        try (var conn = DriverManager.getConnection(System.getProperty("dbUrl"),
+                System.getProperty("dbUser"), System.getProperty("dbPassword"));) {
+            var result = runner.query(conn, request, new ScalarHandler<>());
+            value = String.valueOf(result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+}
 
 
 
